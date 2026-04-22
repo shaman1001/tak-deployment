0dc7d326a7eee4d3ca1f4f239585e31cca3f06c7
shaman1001 <169130629+shaman1001@users.noreply.github.com>
2026-04-22 15:31:17 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-# 1. GCE startup scripts run as 'root', but OpenTAKServer blocks root installation.
-# So, we create a dedicated user 'takadmin' with passwordless sudo access.
+# 1. Create a dedicated user 'takadmin' with passwordless sudo access.
 useradd -m -s /bin/bash takadmin
 usermod -aG sudo takadmin
 echo "takadmin ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/takadmin
 
-# 2. Download and execute the OpenTAKServer installer automatically as 'takadmin'.
-sudo -u takadmin -i sh -c 'curl -s -L https://i.opentakserver.io/ubuntu_installer | bash -'
+# 2. Download the installer script locally
+sudo -u takadmin -i curl -s -L https://i.opentakserver.io/ubuntu_installer -o /home/takadmin/installer.sh
+
+# 3. Patch the installer: Remove hardcoded terminal interactions (/dev/tty)
+sudo -u takadmin -i sed -i 's|</dev/tty||g' /home/takadmin/installer.sh
+sudo -u takadmin -i chmod +x /home/takadmin/installer.sh
+
+# 4. Run the installer non-interactively, answering 'no' to optional prompts (ZeroTier/Mumble)
+sudo -u takadmin -i sh -c 'export DEBIAN_FRONTEND=noninteractive; yes "n" | ./installer.sh'
